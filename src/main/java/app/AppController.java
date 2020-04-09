8ac10232ede809ca252f493a628b8f901bac9b51
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-04-09 14:41:38 +0100

    cache implemented

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -2,6 +2,7 @@ package app;
 
 import entities.Entitie;
 import form.Form;
+import cache.MemoryCache;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -18,9 +19,8 @@ import org.slf4j.LoggerFactory;
 public class AppController {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
-    private String city;
-    private String country;
-    private String state;
+
+    MemoryCache<Entitie> memCache = new MemoryCache<>();
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -36,13 +36,25 @@ public class AppController {
     @PostMapping("/")
     public String greetingSubmit(@ModelAttribute Form form, RestTemplate rt) {
         try {
+            Entitie e = memCache.get(form.getCity(), Entitie.class);
+            if (e != null) {
+                form.setEntitie(e);
+                log.info("DATA FETCHED FROM CACHE");
+                log.info("Expire time: " + memCache.getExpire());
+                return "result";
+            }
+        } catch (final HttpClientErrorException.NotFound e) {
+            log.error(String.valueOf(e.getStatusCode()));
+            log.error((e.getResponseBodyAsString()));
+        }
+        try {
             Entitie airQuality = rt.getForObject("http://api.airvisual.com/v2/city?city="+ form.getCity() + "&state=" + form.getState() + "&country="+ form.getCountry() +"&key=2fa6f39e-db55-44e5-8efa-cf202e452a4b", Entitie.class);
             assert airQuality != null;
-            log.error(airQuality.getStatus());
+            log.info(airQuality.getStatus());
             form.setEntitie(airQuality);
-            log.info(airQuality.toString());
-            log.info(airQuality.getData().toString());
-            log.info(airQuality.getData().getCurrent().getWeather().toString());
+//            log.info(airQuality.getData().getCurrent().getWeather().toString());
+            memCache.put(airQuality.getData().getCity(), airQuality);
+            log.info("DATA FETCHED FROM API");
             return "result";
         }
         catch (final HttpClientErrorException e) {
---
+++ src/main/java/cache/MemoryCache.java
@@ -0,0 +1,78 @@
+package cache;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MemoryCache<Object> {
+
+    private final Map<String, Object> objects;
+    private final Map<String, Long> expire;
+    private final long defaultExpire;
+    private final ExecutorService threads;
+
+    public MemoryCache() {
+        this(100);
+    }
+
+    public MemoryCache(final long defaultExpire) {
+        this.objects = Collections.synchronizedMap(new HashMap<String, Object>());
+        this.expire = Collections.synchronizedMap(new HashMap<String, Long>());
+        this.defaultExpire = defaultExpire;
+        this.threads = Executors.newFixedThreadPool(256);
+        Executors.newScheduledThreadPool(2).scheduleWithFixedDelay(this.removeExpired(), this.defaultExpire / 2, this.defaultExpire, TimeUnit.SECONDS);
+    }
+
+    private Runnable removeExpired() {
+        return new Runnable() {
+            public void run() {
+                for (final String name : expire.keySet()) {
+                    if (System.currentTimeMillis() > expire.get(name)) {
+                        threads.execute(createRemoveRunnable(name));
+                    }
+                }
+            }
+        };
+    }
+
+    private Runnable createRemoveRunnable(final String name) {
+        return new Runnable() {
+            public void run() {
+                objects.remove(name);
+                expire.remove(name);
+            }
+        };
+    }
+
+    public long getExpire() {
+        return this.defaultExpire;
+    }
+
+    public void put(final String name, final Object obj) {
+        this.put(name, obj, this.defaultExpire);
+    }
+
+    public void put(final String name, final Object obj, final long expireTime) {
+        this.objects.put(name, obj);
+        this.expire.put(name, System.currentTimeMillis() + expireTime * 1000);
+    }
+
+    public Object get(final String name) {
+        final Long expireTime = this.expire.get(name);
+        if (expireTime == null) return null;
+            if (System.currentTimeMillis() > expireTime) {
+                this.threads.execute(this.createRemoveRunnable(name));
+                return null;
+            }
+        return Optional.ofNullable(this.objects.get(name)).orElse(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <R extends Object> R get(final String name, final Class<R> type) {
+        return (R) this.get(name);
+    }
+}
